5850e85be0ab764b4eca9789a72c75fe3ea8d9e2
Valentin Alexeev <valentin.alekseev@gmail.com>
2015-07-16 12:49:41 +0300

    Move scb-config to its own node module. SCB is now fully split.

--- app-config.js
+++ app-config.js
@@ -6,25 +6,6 @@
  * - transformation and versioning
  * - outputs
  */
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-
-var routes = require('./config/routes/index');
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'config/views'));
-app.set('view engine', 'jade');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-var oneDay = 86400000;
-
-app.use(express.static(path.join(__dirname, 'config/bower_components'), { maxAge: oneDay }));
-app.use(express.static(path.join(__dirname, 'config/public'), { maxAge: oneDay }));
-
-app.use('/', routes);
+var app = require('scb-config');
 
 module.exports = app;
